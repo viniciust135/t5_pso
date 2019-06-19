117cf26a20c9412ef35d67ad77583b3d2b113912
Vinicius Teixeira <vteixeira@inf.ufsm.br>
2019-06-19 13:03:30 -0300

    Teste que passa

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class YourTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData")
+        pass
+
+    def setUp(self):
+        print("setUp")
+        pass
+
+    def test_false_is_false(self):
+        print("Method: test_false_is_false")
+        self.assertFalse(False)
+
+    def test_one_plus_one_equals_two(self):
+        print("Teste 1+1=2")
+        self.assertEqual(1 + 1, 2)
+
