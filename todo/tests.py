bb63eb68f37b16d9e3d06113fc12b7bfdfcdaa82
Vinicius Teixeira <vteixeira@inf.ufsm.br>
2019-06-19 14:26:05 -0300

    Teste que nao deve passar

--- todo/tests.py
+++ todo/tests.py
@@ -5,18 +5,14 @@ from django.test import TestCase
 class YourTestClass(TestCase):
     @classmethod
     def setUpTestData(cls):
-        print("setUpTestData")
         pass
 
     def setUp(self):
-        print("setUp")
         pass
 
     def test_false_is_false(self):
-        print("Method: test_false_is_false")
-        self.assertFalse(False)
+        self.assertFalse(True)
 
     def test_one_plus_one_equals_two(self):
-        print("Teste 1+1=2")
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(1 + 1, 5)
 
